c2e7538e400c0ab56d5d23c84da90c4483ee6fff
Sanket Giri <sanketgiri48@gmail.com>
2019-08-20 12:46:42 +0530

    Update EmployeeRepository.java

--- springSecurityDemo-2/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ springSecurityDemo-2/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Register;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository<Register, Integer>{
+public interface EmployeeRepository extends CrudRepository<Register, Long>{
 
 }
